6e9b77a2c4e0a52e6bfe3e557b6b14378b4430f1
jackiesogi <jackiesogi@gmail.com>
2023-11-18 03:06:58 +0800

    add async processing

--- main.lua
+++ main.lua
@@ -1,14 +1,39 @@
 local api = require("classifyHandling")
 
-local function assignDirectory(fileName)
+local function assignDirectory(fileName, callback)
     local folderNames = api.loadFolderNames()
-    local folder = api.classifyFolder(folderNames, fileName)
-    local download = {
-        folder = folder,
-        --date = "", -- Add logic to extract creation date here if available
-        name = fileName
-    }
+	print(folderNames)
+    -- 使用 coroutine 模擬異步操作
+    local co = coroutine.create(function()
+        local folder = api.classifyFolder(folderNames, fileName)
+        local download = {
+            folder = folder,
+            name = fileName
+        }
+        -- when operations are finished，call callback function
+        callback(download)
+    end)
 
-    return download
+    -- start coroutine
+    coroutine.resume(co)
 end
+
+local function main()
+    local downloaded_file = "./feature_test/Data_structure_HW1.txt"
+
+    -- callback function
+    local function handleDownload(download)
+        if download and download.folder then
+            print("The downloaded file: '" .. download.name .. "' has been moved to new directory: '" .. download.folder .. "'.")
+        else
+            print("Failed to determine the folder.")
+        end
+    end
+
+    -- call assignDirectory function，and pass callback function as parameter
+    assignDirectory(downloaded_file, handleDownload)
+end
+
+-- entry point
+main()
 
